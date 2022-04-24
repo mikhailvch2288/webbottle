dead5c0ab1a0fbff33762d8a6d6b04dd4622c9d4
Mikhail Vcherashniy <mikhail.vcherashniy@mail.ru>
2022-04-24 17:57:22 +0300

    check tests mail

--- myform.py
+++ myform.py
@@ -3,7 +3,7 @@ import re
 import pdb
 import json
 questions={}
-sh = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})')
+shame = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})')
 
 @post('/home', method='post')
 def my_form():
@@ -11,7 +11,7 @@ def my_form():
     mail = request.forms.get('ADRESS')
     if quest == '' or mail == '':
         return "Some fields are empty. Fill they all"
-    if sh.match(mail) is None:
+    if shame.match(mail) is None:
              return "Invalid mail address"    
 
     questions[mail]=quest
@@ -19,4 +19,17 @@ def my_form():
     with open ('question.txt','w') as txtquest:
         json.dump(questions,txtquest)
     return "Thanks! The answer will be sent to the mail %s" % mail
+    
+
+def checkmail(quest,mail):
+    if quest == '' or mail == '':
+        return "Some fields are empty. Fill they all"
+    if shame.match(mail) is None:
+             return "Invalid mail address"    
+
+    questions[mail]=quest
+    #pdb.set_trace()
+    with open ('question.txt','w') as txtquest:
+        json.dump(questions,txtquest)
+    return None
     
---
+++ test_mail.py
@@ -0,0 +1,21 @@
+import unittest
+from myform import shame, checkmail
+
+class Test_testmail(unittest.TestCase):
+    def test_Bad(self):
+        self.assertFalse(shame.match("worst@..."))
+
+    def test_Good(self):
+        self.assertTrue(shame.match("best@gmail.com"))        
+        
+    def test_Testcheckmail(self):
+        choice = [
+                  ("what a question?","aaa777@mail.ru"),
+                  ("what a problem?","shine35@gmail.com"),
+                  ("Are you have ?","fastback@hotmail.net"),
+                  ("Question?", "qstn@aus.com")
+                  ]                        
+        for(quest,mail) in choice:
+           self.assertIsNone(checkmail(quest, mail))                   
+if __name__ == '__main__':
+    unittest.main()
