4412a2f4c423031dbbeb433bd9151ab796ad83c0
Mikhail Vcherashniy <mikhail.vcherashniy@mail.ru>
2022-05-25 22:30:25 +0300

    сделаны тесты, форма запроса, разметка сайта

--- myform.py
+++ myform.py
@@ -1,25 +1,32 @@
-from bottle import post, request
+from bottle import post, request, redirect
 import re
-import pdb
 import json
-questions={}
+
+company={}
 shame = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})')
+shame2 = re.compile(r'(\A[A-Z]+[a-z])')
 
 @post('/partner', method='post')
 def my_form():
-    quest = request.forms.get('REQUEST')
-    mail = request.forms.get('EMAIL')
-    if quest == '' or mail == '':
-        return "Some fields are empty. Fill they all"
-    if shame.match(mail) is None:
-             return "Invalid mail address"    
 
-    questions[mail]=quest
-    #pdb.set_trace()
-    with open ('question.txt','w') as txtquest:
-        json.dump(questions,txtquest)
-    return "Thanks! The answer will be sent to the mail %s" % mail
+    if(fill_up('REQUEST','EMAIL')is False):
+        return "Заполните все поля!!!"
+
+    description= request.params.REQUEST
+    email = request.params.EMAIL
+
+    company[email]=description
+    with open('partner.txt','w') as pq:
+        json.dump(company,pq)
+    return "Thanks! The application will be sent to the email %s" % email
     
+def fill_up(*words):
+    for i in words:
+        word = request.forms.get(i)
+        if(not word): return False
+
+    return True
+
 
 def checkmail(quest,mail):
     if quest == '' or mail == '':
@@ -27,9 +34,32 @@ def checkmail(quest,mail):
     if shame.match(mail) is None:
              return "Invalid mail address"    
 
-    questions[mail]=quest
+
     #pdb.set_trace()
-    with open ('question.txt','w') as txtquest:
-        json.dump(questions,txtquest)
+    #with open ('question.txt','w') as txtquest:
+        #json.dump(questions,txtquest)
     return None
     
+
+
+#проверка описания
+def isCorrectdescriptionT(description: str):
+    pattern = re.compile(r'(\A[A-Z]+[a-z])')
+    if pattern.match(description):
+        return True
+
+#проверка описания
+def isCorrectdescriptionF(description: str):
+    pattern = re.compile(r'(\A[A-Z]+[a-z])')
+    if pattern.match(description) is None:
+        return False
+#проверка почты
+def isCorrectmailTrue(email: str):
+    pattern = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})')
+    if pattern.match(email):
+        return True
+#проверка почты
+def isCorrectmailFalse(email: str):
+    pattern = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})')
+    if pattern.match(email) is None:
+        return False
---
+++ test_partner.py
@@ -0,0 +1,35 @@
+import unittest
+import myform
+class Test_test_partner(unittest.TestCase):
+    def test_true_description(self):
+        list_description_t = ["our company", "company", "ford motor company", "restaurant chain",
+                        "business partner", "our company has been in existence since 1996"]
+        for y in list_description_t:
+            self.assertTrue(myform.isCorrectdescriptionT(y))
+
+
+    def test_false_description(self):
+        list_description_f = ["рсшыврсыв", "sfdsvfhjljnbdghjkhfbf", "ccccccccccc", "czcdscd@vfsvfsv.cvv",
+                        "HGHGHGHGBCCDNCLASIFOEICOAIODS", "k3;6k3k4kfk44", "   44r4    55u7fsgtr  ", "fddffvcx fdfbd",
+                       "", "      ", "v", "y-10-20", 
+                       "f)", "grrrtr"]
+        for y in list_description_f:
+            self.assertFalse(myform.isCorrectdescriptionF(y))
+
+
+    def test_mail_T(self):
+        list_email_t = ["aaa@mail.ru", "johnwilliams@gmail.com", "moscow23@yahoo.com", "fjdig_cjjd@gmail.com", " rytev-ert@list.ru"]
+        for y in list_email_t:
+            self.assertTrue(myform.isCorrectmailTrue(y))
+
+    def test_mail_F(self):
+        list_email_f = ["@mail.ru", "2222@rd@.cr", "456@bbvvvvv", "", "';.//;", "fe", "hb", "334"]
+        for y in list_email_f:
+            self.assertFalse(myform.isCorrectmailFalse(y))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
